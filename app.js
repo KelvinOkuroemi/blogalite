a8951e7173b4993e462556dae8b921849aa819f9
Kelvin Okuroemi <okuroemidouebikelvin@gmail.com>
2022-08-22 09:10:29 +0100

    add viewport

--- app.js
+++ app.js
@@ -80,10 +80,8 @@ app.post("/compose",(req,res)=>{
 app.listen(process.env.PORT);
 // let port = process.env.PORT;
 // if (port == null || port == "") {
-//   port = 8000;
+//   port = 3000;
 // }
-// app.listen(port);
-
 // app.listen(port, ()=> {
 //   console.log("Server started on host");
 // });
